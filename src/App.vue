45d7d4dfce7bde273e3c19256a297fd4f3d8df7b
maggie <1125479840@qq.com>
2018-11-29 15:39:37 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    startHacking () {
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+}
+</style>
